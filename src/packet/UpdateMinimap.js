37d84986a78931a03eb207d39c6cc9d4a2e0df76
F0RIS <f0risw@gmail.com>
2019-02-14 14:58:07 +0200

    Fixed sending disconnected players

--- src/packet/UpdateMinimap.js
+++ src/packet/UpdateMinimap.js
@@ -1,12 +1,11 @@
 ﻿// Import
 var BinaryWriter = require("./BinaryWriter");
-
+var WebSocket = require("ws");
 
 function UpdateMinimap(playerTracker, clients) {
     this.playerTracker = playerTracker;
     this.clients = clients;
     this.torunament = playerTracker.gameServer.gameMode.IsTournament;
-    this.newFormat = playerTracker.minimapIDs;
 }
 
 module.exports = UpdateMinimap;
@@ -22,28 +21,26 @@ UpdateMinimap.prototype.build = function (protocol) {
 
     var writer = new BinaryWriter();
     writer.writeUInt8(0x43);                                // Packet ID - dec -> 67
-    
+
     var count = 0;
     for (var i = 0; i < clients.length; i++) {
-        if (clients[i].playerTracker.cells.length > 0 &&
+        if (clients[i].playerTracker.socket.readyState == WebSocket.OPEN &&
+            clients[i].playerTracker.cells.length > 0 &&
             clients[i].playerTracker != player &&
-            (clients[i].playerTracker.userID != 0 || this.torunament)){
-                    count++;
-       }
+            (clients[i].playerTracker.userID != 0 || this.torunament)) {
+            count++;
+        }
     }
     writer.writeInt16(count >>> 0);       // Number of elements
 
     for (var i = 0; i < clients.length; i++) {
-        if (clients[i].playerTracker.cells.length > 0 && 
+        if (clients[i].playerTracker.socket.readyState == WebSocket.OPEN &&
+            clients[i].playerTracker.cells.length > 0 &&
             clients[i].playerTracker != player &&
-            (clients[i].playerTracker.userID != 0 || this.torunament)){
+            (clients[i].playerTracker.userID != 0 || this.torunament)) {
             //console.log(clients[i].playerTracker.userID+"  "+clients[i].playerTracker.centerPos.x+":"+clients[i].playerTracker.centerPos.y);
-            if (this.newFormat) {
-                writer.writeInt32(clients[i].playerTracker.userID);
-                writer.writeInt32(clients[i].playerTracker.pID);
-            } else {
-                writer.writeDouble(clients[i].playerTracker.userID);
-            }
+            writer.writeInt32(clients[i].playerTracker.userID);
+            writer.writeInt32(clients[i].playerTracker.pID);
             writer.writeInt16(clients[i].playerTracker.centerPos.x);
             writer.writeInt16(clients[i].playerTracker.centerPos.y);
         }
